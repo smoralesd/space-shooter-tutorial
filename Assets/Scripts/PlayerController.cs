437f650b2e3afce28e0737ac87687cc1715c95b1
Sebastian Morales <sebastian.moralesd@gmail.com>
2015-10-24 16:52:25 -0300

    Enable button to fire the user spaceship weapon  system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,19 @@ public class PlayerController : MonoBehaviour {
     public float tilt;
     public Boundary boundary;
 
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextShot = 0f;
+
+    void Update() {
+        if (Input.GetButton("Fire1") && Time.time > nextShot) {
+            nextShot = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation); 
+        }
+    }
+
     public void FixedUpdate() {
         Rigidbody playerRigidBody = GetComponent<Rigidbody>();
 
