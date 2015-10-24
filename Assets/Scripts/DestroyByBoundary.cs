fe8b2b585d68df814b0eef1eea3637e226a0b4e7
Sebastian Morales <sebastian.moralesd@gmail.com>
2015-10-24 17:07:42 -0300

    Add boundary to destroy objects
    The boundary represent the game's area. Any object exiting the game area
    gets destroyed.

---
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class DestroyByBoundary : MonoBehaviour {
+
+    void OnTriggerExit(Collider other) {
+        Debug.Log("OnTriggerExit"); 
+        Destroy(other.gameObject);
+    }
+}
