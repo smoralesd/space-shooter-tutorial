fac798fc321ea18e917542116dcc3c6295462ff1
Sebastian Morales <sebastian.moralesd@gmail.com>
2015-10-25 08:26:08 -0300

    Add Explosions
    Add explosions for the asteroid being destroyed and the player's
    spaceship being destroyed

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,12 +3,23 @@ using UnityEditor;
 
 public class DestroyByContact : MonoBehaviour {
 
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     public void OnTriggerEnter(Collider other) {
         if (other.tag == "Boundary") {
             return;
         }
 
+        Transform thisTransform = GetComponent<Transform>();
+        Instantiate(explosion, thisTransform.position, thisTransform.rotation);
+
+        if (other.tag == "Player") {
+            Transform playerTransform = other.GetComponent<Transform>();
+            Instantiate(playerExplosion, playerTransform.position, playerTransform.rotation);
+        }
+
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
-}
+};
