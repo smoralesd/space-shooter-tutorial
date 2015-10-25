5f5e926b7551f78e4fa4e166a1e3e57ddda2b77e
Sebastian Morales <sebastian.moralesd@gmail.com>
2015-10-25 09:25:57 -0300

    Spawning Waves
    TODO: destroy by time seems bad (it destroy the asteroids before they
    reach the bottom of the play area) I need to change it

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,14 +5,29 @@ public class GameController : MonoBehaviour {
 
     public GameObject hazard;
     public Vector3 spawnValue;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
 
     public void Start() {
-        SpawnWave();
+        StartCoroutine(SpawnWave());
     }
 
-    public void SpawnWave() {
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
-        Quaternion spawnRotation = Quaternion.identity;
-        Instantiate(hazard, spawnPosition, spawnRotation);
+    public IEnumerator SpawnWave() {
+        yield return new WaitForSeconds(startWait);
+
+        while (true) {
+            
+            for (int i = 0; i < hazardCount; ++i) {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+
+                yield return new WaitForSeconds(spawnWait);
+            }
+
+            yield return new WaitForSeconds(waveWait);
+        }
     }
 }
