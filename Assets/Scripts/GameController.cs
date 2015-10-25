ec498ae1d8df82b842326047d163d4d952ebfbf6
Sebastian Morales <sebastian.moralesd@gmail.com>
2015-10-25 08:49:55 -0300

    Add Basic GameController
    For now, game controller only spawns one asteroid at the beginning of
    the game

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard;
+    public Vector3 spawnValue;
+
+    public void Start() {
+        SpawnWave();
+    }
+
+    public void SpawnWave() {
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+        Quaternion spawnRotation = Quaternion.identity;
+        Instantiate(hazard, spawnPosition, spawnRotation);
+    }
+}
