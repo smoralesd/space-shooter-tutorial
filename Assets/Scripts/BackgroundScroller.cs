38069e8cd7075b6195f7b618db8e1cef555c3c03
Sebastian Morales <sebastian.moralesd@gmail.com>
2015-11-01 09:30:13 -0300

    Star Field & Background
    Add star field (using the particle system)
    Add auto-scrolling to the background to give the player a sense of
    movement.

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class BackgroundScroller : MonoBehaviour {
+
+    public float scrollSpeed;
+    private Vector3 startPosition;
+
+    void Start() {
+        startPosition = transform.position;
+    }
+
+	void Update () {
+        float length = gameObject.transform.localScale.y;
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, length);
+        transform.position = startPosition +  Vector3.forward * newPosition;
+	}
+}
