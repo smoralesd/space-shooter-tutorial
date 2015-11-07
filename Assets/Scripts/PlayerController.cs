d1514570e71366ce40b77fb4ee7fdc08538f61a0
Sebastian Morales <sebastian.moralesd@gmail.com>
2015-11-07 17:52:22 -0300

    Add accelerometer
    Using the accelerometer to control the player spaceship.
    Any touch the player does is translated into a fire action (so the
    spaceship actually fire its weapon)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,7 +18,12 @@ public class PlayerController : MonoBehaviour {
     public Transform shotSpawn;
     public float fireRate;
 
+    private Quaternion calibrationQuaternion;
     private float nextShot = 0f;
+
+    void Start() {
+        CalibrateAccelerometer();
+    }
 
     void Update() {
         if (Input.GetButton("Fire1") && Time.time > nextShot) {
@@ -32,11 +37,9 @@ public class PlayerController : MonoBehaviour {
     public void FixedUpdate() {
         Rigidbody playerRigidBody = GetComponent<Rigidbody>();
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
-
+        Vector3 accelerationRaw = Input.acceleration;
+        Vector3 acceleration = FixedAcceleration(accelerationRaw);
+        Vector3 movement = new Vector3(acceleration.x, 0.0f, acceleration.y);
         playerRigidBody.velocity = speed * movement;
 
         Vector3 currentPosition = playerRigidBody.position;
@@ -45,5 +48,16 @@ public class PlayerController : MonoBehaviour {
         playerRigidBody.position = new Vector3(clampX, 0, clampZ);
 
         GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, playerRigidBody.velocity.x * -tilt);
+    }
+
+    private void CalibrateAccelerometer() {
+        Vector3 accelerationSnapshot = Input.acceleration;
+        Quaternion rotateQuaternion = Quaternion.FromToRotation(new Vector3(0.0f, 0.0f, -1.0f), accelerationSnapshot);
+        calibrationQuaternion = Quaternion.Inverse(rotateQuaternion);
+    }
+
+    private Vector3 FixedAcceleration(Vector3 acceleration) {
+        Vector3 fixedAcceleration = calibrationQuaternion * acceleration;
+        return fixedAcceleration; 
     }
 }
