704a024c2367e473cb015d53ffd659cab76250e4
Sebastian Morales <sebastian.moralesd@gmail.com>
2015-11-07 22:46:54 -0300

    Add basic touch controls
    Add touch pad area to control the spaceship.
    Add touch pad area to fire the weapon.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,15 +18,14 @@ public class PlayerController : MonoBehaviour {
     public Transform shotSpawn;
     public float fireRate;
 
+    public SimpleTouchPad touchPad;
+    public SimpleTouchAreaButton areaButton;
+
     private Quaternion calibrationQuaternion;
     private float nextShot = 0f;
 
-    void Start() {
-        CalibrateAccelerometer();
-    }
-
     void Update() {
-        if (Input.GetButton("Fire1") && Time.time > nextShot) {
+        if (areaButton.CanFire() && Time.time > nextShot) {
             nextShot = Time.time + fireRate;
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
             AudioSource shotAudio = GetComponent<AudioSource>();
@@ -37,9 +36,8 @@ public class PlayerController : MonoBehaviour {
     public void FixedUpdate() {
         Rigidbody playerRigidBody = GetComponent<Rigidbody>();
 
-        Vector3 accelerationRaw = Input.acceleration;
-        Vector3 acceleration = FixedAcceleration(accelerationRaw);
-        Vector3 movement = new Vector3(acceleration.x, 0.0f, acceleration.y);
+        Vector2 direction = touchPad.GetDirection();
+        Vector3 movement = new Vector3(direction.x, 0.0f, direction.y);
         playerRigidBody.velocity = speed * movement;
 
         Vector3 currentPosition = playerRigidBody.position;
@@ -48,16 +46,5 @@ public class PlayerController : MonoBehaviour {
         playerRigidBody.position = new Vector3(clampX, 0, clampZ);
 
         GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, playerRigidBody.velocity.x * -tilt);
-    }
-
-    private void CalibrateAccelerometer() {
-        Vector3 accelerationSnapshot = Input.acceleration;
-        Quaternion rotateQuaternion = Quaternion.FromToRotation(new Vector3(0.0f, 0.0f, -1.0f), accelerationSnapshot);
-        calibrationQuaternion = Quaternion.Inverse(rotateQuaternion);
-    }
-
-    private Vector3 FixedAcceleration(Vector3 acceleration) {
-        Vector3 fixedAcceleration = calibrationQuaternion * acceleration;
-        return fixedAcceleration; 
     }
 }
---
+++ Assets/Scripts/SimpleTouchAreaButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SimpleTouchAreaButton: MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+    private bool touched;
+    private int pointerId;
+    private bool canFire;
+
+    void Awake() {
+        touched = false;
+        canFire = false;
+    }
+
+    public void OnPointerDown(PointerEventData data) {
+        if (!touched) {
+            touched = true;
+            pointerId = data.pointerId;
+            canFire = true;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData data) {
+        if (data.pointerId == pointerId) {
+            touched = false;
+            canFire = false;
+        } 
+    }
+
+    public bool CanFire() {
+        return canFire;
+    }
+}
